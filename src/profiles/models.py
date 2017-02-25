b551add72064492a4814510adec6b17ff055b21e
Trent Hand <livefree1983@gmail.com>
2017-02-25 13:31:37 -0600

    initial folder structure and some templates

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+#profile is used to store the customers information
+class profile(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField(default='description default text')
+
+
+    def __str__(self):
+        return self.name
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#this is my home page, or where the customer will land initially
+def home(request):
+    context = locals()
+    template = 'home.html'
+    return render(request, template, context)
+
+#this is my about page, or where the customer will learn about the company
+def about(request):
+    context = locals()
+    template = 'about.html'
+    return render(request, template, context)
