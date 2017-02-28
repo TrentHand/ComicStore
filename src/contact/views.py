d1e8b67f2e94366cbc50060eb33296d13c4d01a7
Trent Hand <livefree1983@gmail.com>
2017-02-28 13:05:25 -0600

    added contact form

--- src/contact/views.py
+++ src/contact/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .forms import contactForm
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
+#this is my contact page, or where the customer will be able to reach out to the store.
+def contact(request):
+    title = 'Contact'
+    form = contactForm(request.POST or None)
+    confirm_message = None
+#this if statement will print the request in the terminal if the request is valid
+    if form.is_valid():
+        name = form.cleaned_data['name']
+        comment = form.cleaned_data['comment']
+        subject = 'Message from MYSITE.com'
+        message = '%s %s' %(comment, name)
+        emailFrom = form.cleaned_data['email']
+        emailTo = [settings.EMAIL_HOST_USER]
+        send_mail(subject, message, emailFrom, emailTo, fail_silently=True
+        )
+        title = "Thanks!"
+        confirm_message = "Thank you for the message.  We'll get right back to you!"
+        form = None
+
+    context = {'title':title, 'form':form, 'confirm_message':confirm_message,}
+    template = 'contact.html'
+    return render(request, template, context)
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import render
 
 #this is my home page, or where the customer will land initially
 def home(request):
-    context = locals()
+    context = {}
     template = 'home.html'
     return render(request, template, context)
 
 #this is my about page, or where the customer will learn about the company
 def about(request):
-    context = locals()
+    context = {}
     template = 'about.html'
     return render(request, template, context)
